3f9d3cae0ea7dac7d0458ea47ce4975699a8339d
unknown <edouard.michelin15@gmail.com>
2026-08-29 10:17:47 +0200

    fix(profile): fetch data w/ CK user instead of CMD

--- app/Models/ProfileModel.php
+++ app/Models/ProfileModel.php
@@ -25,7 +25,7 @@ class ProfileModel extends Model
             CURLOPT_TIMEOUT => 0,
             CURLOPT_CUSTOMREQUEST => 'GET',
             CURLOPT_HTTPHEADER => array(
-                'XF-Api-User: 33', // Avec l'ID du commandant, pour avoir accès à des champs restreints (ex : groupes) aux utilisateurs non-admin
+                'XF-Api-User: 8', // Avec l'ID du compte Commando Kieffer, pour avoir accès à des champs restreints (ex : groupes) aux utilisateurs non-admin
                 'XF-Api-Key: ' . env("XEN_API_KEY"),
             ),
         ));
